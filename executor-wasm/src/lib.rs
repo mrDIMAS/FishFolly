ac432d73f26d58da5fa6de2ea6ef003a8b8a710c
Dmitry Stepanov <d1maxa@yandex.ru>
2024-01-19 11:50:06 +0300

    added wasm executor

---
+++ executor-wasm/src/lib.rs
@@ -0,0 +1,60 @@
+//! Executor with your game connected to it as a plugin.
+use fish_fall::GameConstructor;
+use fyrox::{
+    core::wasm_bindgen::{self, prelude::*},
+    dpi::LogicalSize,
+    engine::executor::Executor,
+    engine::GraphicsContextParams,
+    event_loop::EventLoop,
+    window::WindowAttributes,
+};
+
+#[wasm_bindgen]
+extern "C" {
+    #[wasm_bindgen(js_namespace = console)]
+    fn error(msg: String);
+
+    type Error;
+
+    #[wasm_bindgen(constructor)]
+    fn new() -> Error;
+
+    #[wasm_bindgen(structural, method, getter)]
+    fn stack(error: &Error) -> String;
+}
+
+fn custom_panic_hook(info: &std::panic::PanicInfo) {
+    let mut msg = info.to_string();
+    msg.push_str("\n\nStack:\n\n");
+    let e = Error::new();
+    let stack = e.stack();
+    msg.push_str(&stack);
+    msg.push_str("\n\n");
+    error(msg);
+}
+
+#[inline]
+pub fn set_panic_hook() {
+    use std::sync::Once;
+    static SET_HOOK: Once = Once::new();
+    SET_HOOK.call_once(|| {
+        std::panic::set_hook(Box::new(custom_panic_hook));
+    });
+}
+
+#[wasm_bindgen]
+pub fn main() {
+    set_panic_hook();
+    let mut window_attributes = WindowAttributes::default();
+    window_attributes.inner_size = Some(LogicalSize::new(1280.0, 720.0).into());
+    window_attributes.title = "Fish Folly".to_string();
+    let mut executor = Executor::from_params(
+        EventLoop::new().unwrap(),
+        GraphicsContextParams {
+            window_attributes,
+            vsync: false,
+        },
+    );
+    executor.add_plugin_constructor(GameConstructor);
+    executor.run()
+}
