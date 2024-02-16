436c17720c87eb2208daa0335b6d17f622915952
Dmitry Stepanov <d1maxa@yandex.ru>
2024-02-16 20:15:11 +0300

    turn on msaa

--- executor/src/main.rs
+++ executor/src/main.rs
@@ -1,9 +1,19 @@
 //! Executor runs the game in standalone (production) mode.
 use fish_fall::GameConstructor;
 use fyrox::engine::executor::Executor;
+use fyrox::engine::GraphicsContextParams;
+use fyrox::event_loop::EventLoop;
 
 fn main() {
-    let mut executor = Executor::new();
+    let mut executor = Executor::from_params(
+        EventLoop::new().unwrap(),
+        GraphicsContextParams {
+            window_attributes: Default::default(),
+            vsync: false,
+            msaa_sample_count: Some(4),
+        },
+    );
+
     executor.add_plugin_constructor(GameConstructor);
     executor.run()
 }
